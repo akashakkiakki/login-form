ce505e9b7b774f39338ff0ecdab7e172a1d61ccd
Akash sharma <akk6928@gmail.com>
2021-07-12 18:11:09 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/Operators';
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
+
+
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient, private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetching=true;
+    this.postsService.FetchPosts().subscribe(posts=>{
+      this.isFetching=false;
+      this.loadedPosts=posts;
+    });
+  }
+
+  onCreatePost(postData: Post) {
+   this.postsService.createAndStorePost(postData.title, postData.content);
+    
+  }
+
+  onFetchPosts() {
+    this.isFetching=true;
+    this.postsService.FetchPosts().subscribe(posts=>{
+      this.isFetching=false;
+      this.loadedPosts=posts;
+    });
+
+  }
+
+  onClearPosts() {
+  this.postsService.deletePosts().subscribe(()=>{
+    this.loadedPosts=[];
+  })
+  }
+
+}
+
+
---
+++ src/app/posts.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from './post.model';
+import { map } from "rxjs/Operators";
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+constructor(private http: HttpClient){}
+
+createAndStorePost(title: string, content: string){
+    const postData: Post = {title: title, content: content}
+    this.http
+    .post<{name: string}>(
+      'https://login-form-24cb2-default-rtdb.firebaseio.com/posts.json',
+      postData
+      )
+     
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+}
+FetchPosts(){
+   return this.http
+    .get<{[key: string]: Post}>('https://login-form-24cb2-default-rtdb.firebaseio.com/posts.json')
+    .pipe(
+      map(responseData=> {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+           postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+        
+      })
+    );
+    
+}
+deletePosts(){
+   return this.http.delete('https://login-form-24cb2-default-rtdb.firebaseio.com/posts.json')
+}
+}
